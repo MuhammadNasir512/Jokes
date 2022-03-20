68cc8dfe133a7bb517f898cd3b63e3df8f67294e
Muhammad Nasir <muhammadnasir512@gmail.com>
2022-03-20 12:48:57 +0000

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Muhammad Nasir Ali on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
